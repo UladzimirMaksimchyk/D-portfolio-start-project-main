17cc05ed49a943da7d5a49531136173eaacb3f2f
Uladzimir <boba70325@gmail.com>
2024-04-13 20:07:39 +0300

    refresh App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 function App() {
     return (
         <div className="App">
-            <Title> IT-INCUBATOR</Title>
+            <Title>Welcome to\ IT-INCUBATOR</Title>
         </div>
     );
 }
