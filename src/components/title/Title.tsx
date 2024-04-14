5e73e79aed2b022f37e28159bd2273bad607ae26
Uladzimir <boba70325@gmail.com>
2024-04-14 14:36:42 +0300

    Added Title.tsx

---
+++ src/components/title/Title.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { styled } from "styled-components";
+
+
+export const Title = () => {
+    return (
+        <StyledTitle>
+            Uladzimir Maksimchyk
+        </StyledTitle>
+
+    )
+
+
+};
+
+const StyledTitle = styled.h2`
+    color: red;
+`
+
+export { }
