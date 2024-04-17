9c6e7dab1f1d9ae2741f1f8aa1a9f3ff6fd6e34d
Uladzimir <boba70325@gmail.com>
2024-04-17 21:15:54 +0300

    Added Work.tsx

---
+++ src/laout/sections/works/work/Work.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export const Work = () => {
+    return(
+<StyledWork>
+    
+</StyledWork>
+    );
+};
+
+const StyledWork = styled.div`
+    
+`
