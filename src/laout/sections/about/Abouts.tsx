dd86557386027a8c9f5707fb736998cc5e89b61a
Uladzimir <boba70325@gmail.com>
2024-04-21 21:01:01 +0300

    Great "Abouts.tsx"

---
+++ src/laout/sections/about/Abouts.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+
+
+type SkillPropsType = {
+    iconId: string
+    title: string
+    description: string
+}
+
+export const Skill = (props: SkillPropsType) => {
+    return (
+
+        <StyledSkill>
+            <Icon iconId={props.iconId} />
+            <SkillTitle>{props.title}</SkillTitle>
+            <SkillText>{props.description}</SkillText>
+        </StyledSkill>
+
+    );
+
+
+};
+
+
+
+
+const StyledSkill = styled.div`
+width: 33%;
+    
+`
+const SkillTitle = styled.h3`
+    
+`
+const SkillText = styled.p`
+        
+    `
+export { }
