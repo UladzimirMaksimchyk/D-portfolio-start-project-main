266936da23ad9cb87a4aadec028c1dcb2e2495d4
Uladzimir <boba70325@gmail.com>
2024-04-22 09:48:46 +0300

    Greate "Slider.tsx"

---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export const Slider = () => {
+    return (
+       
+    );
+
+
+
+    const StyledSlider = styled.div`
+        
+    `
