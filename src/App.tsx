3dee206ada6f0e0b1c33c6ede4475a7d8e7f9759
Uladzimir <boba70325@gmail.com>
2024-04-23 12:33:11 +0300

    Added "Contacts"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import './App.css';
 import styled from 'styled-components';
 import { Header } from './laout/header/Header';
+import { Main } from './laout/sections/main/Main';
+import { Skills } from './laout/sections/skills/Skills';
+import { About } from './laout/sections/about/About';
+import { Works } from './laout/sections/works/Works';
+import { Contacts } from './laout/sections/contacts/Contacts';
 
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <Main/>
+            <Skills/>
+            <About/>
+            <Works/>
+            <Contacts/>
         </div>
     );
 }
