07f4fac1c73a8d060be7d52cf075b96faefd5a1d
Uladzimir <boba70325@gmail.com>
2024-04-24 18:29:28 +0300

    Created "Footer.tsx"

---
+++ src/laout/footer/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import { SectionTitle } from "../../components/SectionTitle";
+
+
+export const Footer = () => {
+    return (
+        <StyledFooter>
+            <SectionTitle>Connect with me:</SectionTitle>
+
+        </StyledFooter>
+    );
+};
+
+const StyledFooter = styled.footer`
+    
+`
+
+
+
+export { }
