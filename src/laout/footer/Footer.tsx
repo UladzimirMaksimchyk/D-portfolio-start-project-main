22fd6840e72e2b935a442ad377a99a997e52f06d
Uladzimir <boba70325@gmail.com>
2024-04-24 18:41:45 +0300

    Added "Copyright"

--- src/laout/footer/Footer.tsx
+++ src/laout/footer/Footer.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-import { SectionTitle } from "../../components/SectionTitle";
+import { Icon } from "../../components/icon/Icon";
 
 
 export const Footer = () => {
     return (
         <StyledFooter>
-            <SectionTitle>Connect with me:</SectionTitle>
-
+            <Name>Uladzimir</Name>
+            <SocialIconList> 
+                <SocialIconLink>
+                    <Icon iconId=""/>
+                </SocialIconLink>
+            </SocialIconList>
+            <Copyright>© 2024 Uladzimir Maksimchyk, All Rights Reserved.</Copyright>
         </StyledFooter>
     );
 };
@@ -15,6 +20,19 @@ export const Footer = () => {
 const StyledFooter = styled.footer`
     
 `
+const Name = styled.span`
+    
+`
+
+const SocialIconList = styled.ul`
+    
+`
+
+
+const SocialIconLink = styled.a`
+        
+`
+
 
 
 
