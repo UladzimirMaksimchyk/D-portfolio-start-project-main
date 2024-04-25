dd9f53d53fb6c4ffb8cddce3cf170838611f8fc0
Uladzimir <boba70325@gmail.com>
2024-04-25 16:05:19 +0300

    Added "section:"

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -1,13 +1,37 @@
 import { createGlobalStyle } from "styled-components";
+import { theme } from "./Theme";
 
 export const GlobalStyle = createGlobalStyle`
+*,
+*::before,
+*::after{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
 body {
   margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+a {
+    text-decoration: none;
+
+}
+ul {
+    list-style: none;
+}
+button {
+    background-color: unset;
+    border: none;
+}
+section: nth-of-type(odd) {
+  background-color: ${theme.colors.primaryBg};
+  
 }
 `
 
