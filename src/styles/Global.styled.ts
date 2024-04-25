a37c0db33c6311d6ef846ba1f7913d93f44c1bab
Uladzimir <boba70325@gmail.com>
2024-04-25 16:09:40 +0300

    Added "section: ...(even)"

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -31,7 +31,10 @@ button {
 }
 section: nth-of-type(odd) {
   background-color: ${theme.colors.primaryBg};
-  
 }
+section: nth-of-type(even) {
+  background-color: ${theme.colors.secondaryBg};
+}
+
 `
 
