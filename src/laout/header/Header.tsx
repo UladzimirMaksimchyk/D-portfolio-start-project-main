b1cfe11bd826c607314ffeded7777c177430b629
Uladzimir <boba70325@gmail.com>
2024-04-28 21:18:10 +0300

    Added "FlexWraper"

--- src/laout/header/Header.tsx
+++ src/laout/header/Header.tsx
@@ -4,6 +4,7 @@ import { Logo } from "../../components/logo/Logo";
 import { Menu } from "../../components/menu/Menu";
 import { Title } from "../../components/title/Title";
 import { Container } from "../../components/Container";
+import { FlexWrapper } from "../../components/FlexWrapper";
 
 const items = ["Home", "About me", "Projects", "Contact"]
 
@@ -11,9 +12,12 @@ export const Header = () => {
     return (
         <StyledHeader>
             <Container>
+                <FlexWrapper >
                 {/* <Logo /> */}
                 <Title />
                 <Menu menuItems={items} />
+
+                </FlexWrapper>
 
             </Container>
 
