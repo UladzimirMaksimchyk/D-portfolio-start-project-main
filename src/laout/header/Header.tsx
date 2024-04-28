90a553eae83615fd10c831ec3cbd7316254e0108
Uladzimir <boba70325@gmail.com>
2024-04-28 21:15:57 +0300

    Added "Container" in "Header.tsx"

--- src/laout/header/Header.tsx
+++ src/laout/header/Header.tsx
@@ -3,17 +3,25 @@ import { styled } from "styled-components";
 import { Logo } from "../../components/logo/Logo";
 import { Menu } from "../../components/menu/Menu";
 import { Title } from "../../components/title/Title";
+import { Container } from "../../components/Container";
+
+const items = ["Home", "About me", "Projects", "Contact"]
+
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo />
-          <Title/>
-            <Menu />
+            <Container>
+                {/* <Logo /> */}
+                <Title />
+                <Menu menuItems={items} />
+
+            </Container>
 
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
-    background-color: green;
+display: flex;
+justify-content: space-around;
 `
