dbc38cfc6e85e9a20b15e1c78b3bb022cffd7bff
Uladzimir <boba70325@gmail.com>
2024-04-30 11:04:11 +0300

    Created const "Mask"

--- src/laout/header/headerMenu/HeaderMenu.tsx
+++ src/laout/header/headerMenu/HeaderMenu.tsx
@@ -2,18 +2,27 @@ import React from "react";
 import styled from "styled-components";
 
 
-export const HeaderMenu = (props: {menuItems: Array<string>}) => {
-  return(
-<StyledHeaderMenu>
-    <ul>
-        {props.menuItems.map((item ,index)=>{
-return <ListItem key={index}>
-    <Link href="">{item}</Link>
-</ListItem>
+export const HeaderMenu = (props: { menuItems: Array<string> }) => {
+  return (
+    <StyledHeaderMenu>
+      <ul>
+        {props.menuItems.map((item, index) => {
+          return <ListItem key={index}>
+            <Link href="">
+              {item}
+              <Mask>
+                <span>{item}</span>
+              </Mask>
+              <Mask>
+                <span>{item}</span>
+              </Mask>
+
+              </Link>
+          </ListItem>
         })}
 
-    </ul>
-</StyledHeaderMenu>
+      </ul>
+    </StyledHeaderMenu>
   );
 };
 
@@ -27,4 +36,18 @@ ul{
     gap: 30px;
 
 }
+`
+const ListItem = styled.li`
+  
+`
+const Link = styled.a`
+  font-family: Poppins,sans-serif;
+font-size: 20px;
+font-weight: 600;
+text-align: center;
+color:red;
+
+`
+const Mask = styled.span`
+  
 `
