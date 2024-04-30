d79aff7a61167aa056994ac5f78903cf08145128
Uladzimir <boba70325@gmail.com>
2024-04-30 10:38:59 +0300

    Added "List Item"

--- src/laout/header/headerMenu/HeaderMenu.tsx
+++ src/laout/header/headerMenu/HeaderMenu.tsx
@@ -4,28 +4,27 @@ import styled from "styled-components";
 
 export const HeaderMenu = (props: {menuItems: Array<string>}) => {
   return(
-<StyledMenu>
+<StyledHeaderMenu>
     <ul>
         {props.menuItems.map((item ,index)=>{
-return <li key={index}>
-    <a href="">{item}</a>
-</li>
+return <ListItem key={index}>
+    <Link href="">{item}</Link>
+</ListItem>
         })}
 
     </ul>
-</StyledMenu>
+</StyledHeaderMenu>
   );
 };
 
 
 
 
-const StyledMenu = styled.nav`
+const StyledHeaderMenu = styled.nav`
 color: black;
 ul{
     display: flex;
     gap: 30px;
 
 }
-   
 `
