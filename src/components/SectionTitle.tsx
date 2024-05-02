012ec657a6be5f11c53c7af2f27fa2c2e75439bf
Uladzimir <boba70325@gmail.com>
2024-05-02 10:59:01 +0300

    Update "SectionTitle.tsx"

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 
 
-const SectionTitle = styled.h2`
-    
+export const SectionTitle = styled.h2`
+text-align: center;
+font-family: Poppins;
+font-size: 32px;
+font-weight: 600;
+line-height: 48px;
+
 `
 
+export {}
