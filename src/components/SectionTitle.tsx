34a8b2d12bded97f80001215dfe26b43cf2fa293
Uladzimir <boba70325@gmail.com>
2024-05-02 11:14:45 +0300

    Update "SectionTitle.tsx"

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,13 +1,30 @@
 import styled from "styled-components";
+import { theme } from "../styles/Theme";
 
 
 export const SectionTitle = styled.h2`
 text-align: center;
-font-family: Poppins;
+font-family: Poppins, sans-serif;
 font-size: 32px;
 font-weight: 600;
 line-height: 48px;
+margin-bottom: 90px;
 
+position: relative;
+
+&::before{
+    content: "";
+    display: inline-block;
+    width: 155px;
+    height: 3px;
+    background-color: #ff00f7;
+    
+    position: absolute;
+    left: 50%;
+    bottom: -10px;
+    transform: translateX(-50%);
+
+}
 `
 
 export {}
