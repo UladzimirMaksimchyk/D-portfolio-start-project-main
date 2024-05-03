a96e8460cbe836efed60eb8eb9e5a4ea289ce6d6
Uladzimir <boba70325@gmail.com>
2024-05-03 15:49:56 +0300

    Update "Slider.tsx"

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,22 +1,61 @@
 import React from "react";
 import styled from "styled-components";
+import { Icon } from "../icon/Icon";
+import { FlexWrapper } from "../FlexWrapper";
 
+type SliderPropsType = {
+    iconId: string
+}
 
-export const Slider = () => {
+export const Slider = (props: SliderPropsType) => {
     return (
         <StyledSlider>
-
+            <FlexWrapper>
+                <Slide>
+                    <Icon iconId={props.iconId} />
+                    <Name>UI & UX DESIGNING</Name>
+                </Slide>
+            </FlexWrapper>
+            <Pagination>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </Pagination>
         </StyledSlider>
-       
+
     )
 };
 
 
 
-    const StyledSlider = styled.div`
-        
+const StyledSlider = styled.div`
+ border: 1px solid red;
+ max-width: 500px;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ `
+const Slide = styled.div`
+       text-align: start;
     `
-    const Slide = styled.div`
-        
+
+const Name = styled.h3`
+font-family: Poppins;
+font-size: 24px;
+font-weight: 600;
+text-align: left;
+
     `
-    export{}
+const Pagination = styled.span`
+span {
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    margin: 5px;
+    background-color: red;
+}
+    
+`
+
+export { }
