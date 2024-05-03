5d48a5f8e720c31249a09b706e5385c1159972f7
Uladzimir <boba70325@gmail.com>
2024-05-03 16:07:30 +0300

    Added classname "active"

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Icon } from "../icon/Icon";
 import { FlexWrapper } from "../FlexWrapper";
+import { theme } from "../../styles/Theme";
 
 type SliderPropsType = {
     iconId: string
@@ -18,7 +19,7 @@ export const Slider = (props: SliderPropsType) => {
             </FlexWrapper>
             <Pagination>
                 <span></span>
-                <span></span>
+                <span className={"active"}></span>
                 <span></span>
                 <span></span>
             </Pagination>
@@ -37,23 +38,34 @@ const StyledSlider = styled.div`
  align-items: center;
  `
 const Slide = styled.div`
-       text-align: start;
+text-align: center;
     `
 
-const Name = styled.h3`
+const Name = styled.span`
 font-family: Poppins;
 font-size: 24px;
 font-weight: 600;
-text-align: left;
+letter-spacing: 1px;
+text-transform: uppercase;
+margin: 22px 0 42px;
+display: inline-block;
+`
 
-    `
-const Pagination = styled.span`
+
+const Pagination = styled.div`
+
 span {
     display: inline-block;
-    width: 20px;
-    height: 20px;
-    margin: 5px;
-    background-color: red;
+    width: 7px;
+    height: 7px;
+    background-color: #000000;
+    /* background-color: ${theme.colors.font}; */
+    border-radius: 20px;
+
+
+    & + span {
+margin-left: 5px;
+    }
 }
     
 `
