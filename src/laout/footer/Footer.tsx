f7c304951aa2971fd05d24e9e83f14f261446637
Uladzimir <boba70325@gmail.com>
2024-05-05 11:40:47 +0300

    Delete min-height to the "Footer.tsx"

--- src/laout/footer/Footer.tsx
+++ src/laout/footer/Footer.tsx
@@ -1,38 +1,65 @@
 import React from "react";
 import styled from "styled-components";
 import { Icon } from "../../components/icon/Icon";
+import { FlexWrapper } from "../../components/FlexWrapper";
 
 
 export const Footer = () => {
     return (
         <StyledFooter>
-            <Name>Uladzimir</Name>
-            <SocialIconList> 
-                <SocialIconLink>
-                    <Icon iconId=""/>
-                </SocialIconLink>
-            </SocialIconList>
-            <Copyright>© 2024 Uladzimir Maksimchyk, All Rights Reserved.</Copyright>
+            <FlexWrapper direction={"column"} align={"center"}>
+                <Name>Uladzimir</Name>
+                <SocialList>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon height={"21px"} width={"21px"} viewbox={"0 0 21px 21px"} iconId={"git"} />
+                        </SocialLink>
+                    </SocialItem>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon height={"21px"} width={"21px"} viewbox={"0 0 21px 21px"} iconId={"git"} />
+                        </SocialLink>
+                    </SocialItem>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon height={"21px"} width={"21px"} viewbox={"0 0 21px 21px"} iconId={"git"} />
+                        </SocialLink>
+                    </SocialItem>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon height={"21px"} width={"21px"} viewbox={"0 0 21px 21px"} iconId={"git"} />
+                        </SocialLink>
+                    </SocialItem>
+                </SocialList>
+                <Copyright>© 2024 Uladzimir Maksimchyk, All Rights Reserved.</Copyright>
+
+            </FlexWrapper>
         </StyledFooter>
     );
 };
 
 const StyledFooter = styled.footer`
-    
 `
 const Name = styled.span`
     
 `
 
-const SocialIconList = styled.ul`
+const SocialList = styled.ul`
+    display: flex;
+    gap: 30px;
+`
+const SocialItem = styled.li`
     
 `
 
 
-const SocialIconLink = styled.a`
+const SocialLink = styled.a`
         
 `
 
+const Copyright = styled.small`
+        
+`
 
 
 
