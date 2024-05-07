9247a468265d6faa6ed4089f48c456ab97c0f009
Uladzimir <boba70325@gmail.com>
2024-05-07 21:00:59 +0300

    Update "secondaryBg"

---
+++ src/styles/Theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+    colors:{
+        primaryBg: "#ffffff",
+        secondaryBg:"#0dff11",
+        accent: "#ff00f7",
+
+        font: "rgba(#000000)",
+        placeholderColor: "#000000"
+    }
+}
+export {}
