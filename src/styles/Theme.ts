6b2577f63652feab22f26f1ff7ff801e5d1c0658
Uladzimir <boba70325@gmail.com>
2024-05-10 19:40:52 +0300

    Created "media"

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -2,9 +2,16 @@ export const theme = {
     colors:{
         primaryBg: "#ffffff",
         accent: "#ff00f7",
-
+        secondaryBg:"#000000",
         font: "rgba(#000000)",
         placeholderColor: "#000000"
     }
+
+    media:{
+        
+    }
 }
+
+
+
 export {}
