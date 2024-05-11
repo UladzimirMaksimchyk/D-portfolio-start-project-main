1c36d5e8310124f425c59ef93a566f7dd874779d
Uladzimir <boba70325@gmail.com>
2024-05-11 18:15:12 +0300

    Update "MobileMenu.tsx"

---
+++ src/laout/header/mobileMenu/MobileMenu.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../../../styles/Theme";
+
+
+export const MobileMenu = (props: { menuItems: Array<string> }) => {
+  return (
+    <StyledMobileMenu>
+      <ul>
+        {props.menuItems.map((item, index) => {
+          return <ListItem key={index}>
+            <Link href="">
+              {item}
+              {/* <Mask>
+                <span>{item}</span>
+              </Mask>
+              <Mask>
+                <span>{item}</span>
+              </Mask> */}
+
+            </Link>
+          </ListItem>
+        })}
+      </ul>
+    </StyledMobileMenu>
+  );
+};
+
+
+
+
+const StyledMobileMenu = styled.nav`
+ul{
+display: flex;
+gap: 30px;
+justify-content: center;
+}
+
+@media ${theme.media.tablet}{
+  display: none;
+}
+
+`
+const Link = styled.a`
+font-family: Poppins,sans-serif;
+font-size: 20px;
+font-weight: 600;
+text-align: center;
+color:#ff00f7;
+`
+
+
+const ListItem = styled.li`
+  &:hover{
+  transform: scale(1.2);
+}
+`
+
+
