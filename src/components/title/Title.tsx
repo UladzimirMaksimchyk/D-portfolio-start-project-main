fc0bcb64ea6720f394889d8e13a88c068729c834
Uladzimir <boba70325@gmail.com>
2024-05-13 13:13:53 +0300

    Update "StyledTitle" to the "Title.tsx"

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { styled } from "styled-components";
+import { theme } from "../../styles/Theme";
+import { font } from "../../styles/Common";
 
 
 export const Title = () => {
     return (
         <StyledTitle>
-            Uladzimir Maksimchyk
+            Uladzimir <span>Maksimchyk</span> 
         </StyledTitle>
 
     )
@@ -14,14 +16,30 @@ export const Title = () => {
 };
 
 const StyledTitle = styled.h2`
-    color: black;
-    font-family: Poppins;
-font-size: 28px;
-font-weight: 600;
-line-height: 42px;
-text-align: left;
-text-shadow: 25px 15px 6px rebeccapurple;
-;
+ ${font({ family: "'Poppins', sans-serif", weight: 600, Fmax: 28, Fmin: 20 })} 
+    color:${theme.colors.font};
+    /* font-family: Poppins;
+    font-size: 28px;
+    font-weight: 600; */
+
+
+
+span {
+position: relative;
+z-index: 0;
+
+&::before{
+    content: "";
+    display: inline-block;
+    width: 100%;
+    height: 8px;
+    background-color: #ff00f7;
+    position: absolute;
+    bottom: 0;
+    z-index: -1;
+}
+}
+
 `
 
 
