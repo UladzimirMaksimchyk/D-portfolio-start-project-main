323474110d9702aaf5db3b47c177cb5b788dde3d
Uladzimir <boba70325@gmail.com>
2024-05-15 20:25:11 +0300

    Update const "Header"

--- src/laout/header/Header.tsx
+++ src/laout/header/Header.tsx
@@ -5,32 +5,28 @@ import { Title } from "../../components/title/Title";
 import { Container } from "../../components/Container";
 import { FlexWrapper } from "../../components/FlexWrapper";
 import { HeaderMenu } from "./headerMenu/HeaderMenu";
-
+import { theme } from "../../styles/Theme";
+import { MobileMenu } from "./mobileMenu/MobileMenu";
 
 const items = ["Home", "About me", "Projects", "Contact"]
 
-export const Header = () => {
+export const Header: React.FC = () => {
     return (
         <StyledHeader>
-
             <Container>
-
                 <FlexWrapper justify={"space-between"} align={"center"}>
-
                     {/* <Logo /> */}
                     <Title />
                     <HeaderMenu menuItems={items} />
-
+                    <MobileMenu menuItems={items} />
                 </FlexWrapper>
-
             </Container>
-
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
-background-color:snow;
+background-color: ${theme.colors.primaryBg};
 padding: 0;
 position: fixed;
 top: 0;
